b1de5d3bcf648d899e1524de661f62911acbc093
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-08-31 16:28:33 -0700

    Daylist.js is implemented to render list of days, and corresponding component is on storybook

---
+++ src/components/DayList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props){
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  const dayList = props.days.map((day=>{
+    return(
+      <DayListItem 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.day}
+      setDay={props.setDay}  />
+    )
+  }))
+  return dayList;
+}
