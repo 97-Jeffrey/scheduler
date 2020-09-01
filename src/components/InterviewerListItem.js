bee245f0221abe8dff06362d5db25e13a18ae81f
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-08-31 23:25:36 -0700

    interviewlistitem component is created with styles and test codes for storybook

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+const classNames = require('classnames');
+
+export default function InterviewerListItem(props) {
+  const InterviewerListItemClass = classNames( {
+    'interviewers__item':true,
+    "interviewers__item--selected ": props.selected,
+    "interviewers__item-image": props.avatar,
+    // "interviewers__item--selected-image": props.avator && props.selected
+  })
+  return (
+
+    <li className={InterviewerListItemClass}>
+      <img
+        className={InterviewerListItemClass}
+        src={props.avatar}
+        alt={props.name}
+        selected
+        onClick={props.setInterviewer}
+      />
+       {props.selected &&<>{props.name}</>}
+    </li>
+  )
+
+}
