f0fd2ed6c495bf5fecfd6b81ca93c5e9dc85897e
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-09-01 15:53:31 -0700

    more files are created for appointment part, and their stories are added into index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
+import Show from './Show';
+import Confirm from "./Confirm";
 
 export default function Appointment(props){
   return(
