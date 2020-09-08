c5102111218af5bbab081bc8750c4d7b1f8b4baf
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-09-08 00:09:41 -0700

    the axios file is setup for mock data, hte old test from applicaiton.test.js is removed, __mocks__ folder is for placing the mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -36,22 +36,22 @@ export default function Application(props) {
     )
   })
 
-  useEffect(() => {
-    Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`)
-    ])
-      .then(all => {
-        console.log(all)
-        setState(prev => ({
-          ...prev,
-          days: all[0].data,
-          appointments: all[1].data,
-          interviewers: all[2].data,
-        }))
-      })
-  }, [])
+  // useEffect(() => {
+  //   Promise.all([
+  //     axios.get(`/api/days`),
+  //     axios.get(`/api/appointments`),
+  //     axios.get(`/api/interviewers`)
+  //   ])
+  //     .then(all => {
+  //       console.log(all)
+  //       setState(prev => ({
+  //         ...prev,
+  //         days: all[0].data,
+  //         appointments: all[1].data,
+  //         interviewers: all[2].data,
+  //       }))
+  //     })
+  // }, [])
   
  
   return (
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -66,7 +66,22 @@ export function useApplicationData() {
 
   }
 
-
+  useEffect(() => {
+    Promise.all([
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`)
+    ])
+      .then(all => {
+        console.log(all)
+        setState(prev => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        }))
+      })
+  }, [])
 
   return { state, setDay, setState, bookInterview, cancelInterview };
 
