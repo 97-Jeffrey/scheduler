da2d03d5168a083fd8af700b15d79955f3114a0e
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-09-09 15:52:02 -0700

    the folder is created with content manually deleted and then inserted with two tests

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,15 @@
+const { iteratee } = require("lodash")
+const { CYCLIC_KEY } = require("@storybook/addon-actions/dist/constants")
+
+describe("Navigation",()=>{
+  it('should visit root',()=>{
+    cy.visit("/");
+  })
+  it("should navigate to Tuesday",()=>{
+    cy.visit("/");
+    cy.contains("div", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected")
+
+  })
+})
